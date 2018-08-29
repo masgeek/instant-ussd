5950048c65b9cb48e18a87e62e43a4ebf5f61d0e
Sammy Barasa <barsamms@gmail.com>
2018-08-29 14:22:35 +0300

    Switched from ArrayObject to array
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- src/UssdEvent.php
+++ src/UssdEvent.php
@@ -13,7 +13,8 @@ use ArrayObject;
  *
  * @author David Bwire
  */
-class UssdEvent extends Event {
+class UssdEvent extends Event
+{
 
     /**
      *
@@ -23,11 +24,12 @@ class UssdEvent extends Event {
 
     /**
      * Get ServicelocatorInterface that is already set or try accessing it from event target
-     * 
+     *
      * @return ServiceLocatorInterface
      * @throws \Exception
      */
-    public function getServiceLocator() {
+    public function getServiceLocator()
+    {
         if ($this->serviceLocator instanceof ServiceLocatorInterface) {
             return $this->serviceLocator;
         } elseif (method_exists($this->getTarget(), 'getServiceLocator')) {
@@ -43,56 +45,62 @@ class UssdEvent extends Event {
     }
 
     /**
-     * 
+     *
      * @param ServiceLocatorInterface $serviceLocator
      * @return $this
      */
-    public function setServiceLocator(ServiceLocatorInterface $serviceLocator) {
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
         $this->serviceLocator = $serviceLocator;
         return $this;
     }
 
     /**
-     * 
+     *
      * @return bool
      */
-    public function containsIncomingData() {
+    public function containsIncomingData()
+    {
 
         $isIncomingData = $this->getParam('is_incoming_data', false);
         return ($isIncomingData === true);
     }
 
     /**
-     * 
+     *
      * @return mixed string|null
      */
-    public function getLatestResponse() {
+    public function getLatestResponse()
+    {
         return $this->getParam('latest_response', null);
     }
 
     /**
-     * 
+     *
      * @return mixed string|null
      */
-    public function getFirstResponse() {
+    public function getFirstResponse()
+    {
         return $this->getParam('first_response', null);
     }
 
     /**
-     * 
+     *
      * @return mixed array|null
      */
-    public function getNonExtraneousValues() {
+    public function getNonExtraneousValues()
+    {
         return $this->getParam('a_values_non_extraneous');
     }
 
     /**
      * Returns the class (essentially the controller) which initialized InstantUssd class
-     * 
+     *
      * @return object
      * @throws Exception
      */
-    public function getInitializer() {
+    public function getInitializer()
+    {
         $eventTarget = $this->target;
         if (gettype($eventTarget) === 'object') {
             return $eventTarget->getInitializer();
@@ -103,31 +111,32 @@ class UssdEvent extends Event {
 
     /**
      * Retreives an instance of InstantUssd from which UssdEvent was triggered or throws an error
-     * 
+     *
      * @return InstantUssd
      * @throws Exception
      */
-    public function getInstantUssd() {
+    public function getInstantUssd()
+    {
         $eventTarget = $this->target;
         if (is_object($eventTarget) && ($eventTarget instanceof InstantUssd)) {
             return $eventTarget;
         }
-        throw new Exception("UssdEvent was not triggered"
-        . " from InstantUssd class");
+        throw new Exception("UssdEvent was not triggered from InstantUssd class");
     }
 
     /**
-     * 
-     * @param ArrayObject $menuConfig
+     *
+     * @param array $menuConfig
      */
-    public function attachDynamicErrors(ArrayObject $menuConfig) {
+    public function attachDynamicErrors(array $menuConfig)
+    {
 
-        $errorMessage            = $this->getParam('error_message', "");
+        $errorMessage = $this->getParam('error_message', "");
         $menuConfig['has_error'] = !$this->getParam('is_valid', true);
 
         $defaultErrorMessage = array_key_exists('error_message', $menuConfig) ? $menuConfig['error_message'] : "";
         if ($menuConfig['has_error'] &&
-                empty($defaultErrorMessage)) {
+            empty($defaultErrorMessage)) {
             $menuConfig['error_message'] = $errorMessage;
         }
     }
