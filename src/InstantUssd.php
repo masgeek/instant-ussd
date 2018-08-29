25f3ce3db56a802b0fb22d6a64cc1a880a72e2bc
Sammy Barasa <barsamms@gmail.com>
2018-08-29 14:14:30 +0300

    Switched from ArrayObject to array
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- src/InstantUssd.php
+++ src/InstantUssd.php
@@ -22,29 +22,30 @@ use ArrayObject;
  *
  * @author David Bwire
  */
-class InstantUssd {
+class InstantUssd
+{
 
     /**
      *
-     * @var UssdService 
+     * @var UssdService
      */
     protected $ussdService;
 
     /**
      *
-     * @var UssdResponseGenerator 
+     * @var UssdResponseGenerator
      */
     protected $ussdResponseGenerator;
 
     /**
      *
-     * @var ServiceLocatorInterface 
+     * @var ServiceLocatorInterface
      */
     protected $serviceLocator;
 
     /**
      *
-     * @var ArrayObject 
+     * @var ArrayObject
      */
     protected $instantUssdConfig;
 
@@ -56,50 +57,51 @@ class InstantUssd {
 
     /**
      *
-     * @var object 
+     * @var object
      */
     protected $logger;
 
     /**
      *
-     * @var string 
+     * @var string
      */
     protected $ussdEventManager;
 
     /**
      *
-     * @var EventManager 
+     * @var EventManager
      */
     protected $eventManager;
 
     /**
      *
-     * @var ArrayObject 
+     * @var ArrayObject
      */
     protected $ussdMenusConfig;
 
     /**
-     * 
-     * @param ArrayObject $instantUssdConfig
+     *
+     * @param array $instantUssdConfig
      * @param object $initializer The class that instantiates this class (InstantUssd)
      *               The $initializer is useful when you'd like to extract dependancies
      *               for use in a ussd event listener
-     * 
+     *
      * @throws Exception
      */
-    public function __construct(ArrayObject $instantUssdConfig, $initializer) {
+    public function __construct(array $instantUssdConfig, $initializer)
+    {
         if (gettype($initializer) != 'object') {
             throw new Exception('The initializer must be an object');
         }
-        $this->initializer       = $initializer;
+        $this->initializer = $initializer;
         $this->instantUssdConfig = $instantUssdConfig;
-        $ussdResponseGenerator   = new UssdResponseGenerator();
+        $ussdResponseGenerator = new UssdResponseGenerator();
 
-        $ussdMenusConfig   = $instantUssdConfig['ussd_menus'];
+        $ussdMenusConfig = $instantUssdConfig['ussd_menus'];
         $ussdEventManager = $instantUssdConfig['ussd_event_manager'];
         $this->setUssdEventManager($ussdEventManager);
 
-        $this->ussdMenusConfig       = $ussdMenusConfig;
+        $this->ussdMenusConfig = $ussdMenusConfig;
         $this->ussdResponseGenerator = $ussdResponseGenerator;
 
         // IMPORTANT - force $this->eventManager to hold a configured EventManager instance
@@ -107,28 +109,30 @@ class InstantUssd {
     }
 
     /**
-     * 
+     *
      * @param array $ussdData
      * @param string $errorMessage
      * @return Response
      */
-    public function showError(array $ussdData, $errorMessage = null) {
+    public function showError(array $ussdData, $errorMessage = null)
+    {
         $ussdData['error_message'] = $errorMessage;
-        $results                   = $this->eventManager->triggerUntil(function($result) {
+        $results = $this->eventManager->triggerUntil(function ($result) {
             return ($result instanceof Response);
         }, '_error_', $this, $ussdData);
         return $results->last();
     }
 
     /**
-     * 
+     *
      * @param array $ussdData
      * @return Response|void
      */
-    public function exitUssd(array $ussdData) {
+    public function exitUssd(array $ussdData)
+    {
 
         // trigger until we get a Response
-        $results = $this->eventManager->triggerUntil(function($result) {
+        $results = $this->eventManager->triggerUntil(function ($result) {
             return ($result instanceof Response);
         }, '_exit_', $this, $ussdData);
 
@@ -141,12 +145,13 @@ class InstantUssd {
     }
 
     /**
-     * 
+     *
      * @param array $ussdData
      * @param string $homePageMenuId
      * @return Response
      */
-    public function showHomePage(array $ussdData, $homePageMenuId) {
+    public function showHomePage(array $ussdData, $homePageMenuId)
+    {
         // enforce home page format
         if (!(substr($homePageMenuId, 0, 5) === 'home_')) {
             throw new Exception("Home pages must begin with 'home_'");
@@ -165,25 +170,26 @@ class InstantUssd {
     }
 
     /**
-     * 
+     *
      * @param array $ussdData
      * @param string $nextScreenId
      * @return Response
      */
-    public function showNextScreen(array $ussdData, $nextScreenId) {
+    public function showNextScreen(array $ussdData, $nextScreenId)
+    {
         // with the next_screen
         // disable incoming cycle
         $ussdData['is_incoming_data'] = false;
         // don't use triggerUntil as it will prevent tracking
         // try and render next menu
-        $outGoingCycleResults         = $this->eventManager->trigger($nextScreenId, $this, $ussdData);
-        $outGoingCycleResult          = $outGoingCycleResults->first();
+        $outGoingCycleResults = $this->eventManager->trigger($nextScreenId, $this, $ussdData);
+        $outGoingCycleResult = $outGoingCycleResults->first();
         // Try and find a response that's not a skippable
         while ($outGoingCycleResult instanceof UssdMenuItem) {
             // get the next menu
-            $nextScreenId         = $outGoingCycleResult->getNextMenuId();
+            $nextScreenId = $outGoingCycleResult->getNextMenuId();
             $outGoingCycleResults = $this->eventManager->trigger($nextScreenId, $this, $ussdData);
-            $outGoingCycleResult  = $outGoingCycleResults->first();
+            $outGoingCycleResult = $outGoingCycleResults->first();
         }
         //--- send data to user
         if ($outGoingCycleResult instanceof Response) {
@@ -194,13 +200,14 @@ class InstantUssd {
     }
 
     /**
-     * 
+     *
      * @param array $ussdData
      * @return mixed Response|false
      */
-    public function goBack(array $ussdData) {
+    public function goBack(array $ussdData)
+    {
         // try and find the previous menu
-        $results      = $this->eventManager->trigger('_go_back_.pre', $this, $ussdData);
+        $results = $this->eventManager->trigger('_go_back_.pre', $this, $ussdData);
         // retreive first data
         $previousMenu = $results->first();
 
@@ -228,92 +235,100 @@ class InstantUssd {
 
     /**
      * This method provides UssdEvent with a way to access the service manager
-     * 
+     *
      * @return ServiceLocatorInterface
      */
-    public function getServiceLocator() {
+    public function getServiceLocator()
+    {
         $serviceLocator = $this->serviceLocator;
         if (!$serviceLocator instanceof ServiceManager) {
             if (!array_key_exists('service_manager', $this->instantUssdConfig)) {
                 throw new Exception('service_manager key missing on instant_ussd config.');
             }
-            $serviceManagerConfig                       = $this->instantUssdConfig['service_manager'];
+            $serviceManagerConfig = $this->instantUssdConfig['service_manager'];
             // attach config for use by dbAdapter
             $serviceManagerConfig['services']['config'] = $this->instantUssdConfig;
-            $this->serviceLocator                       = new ServiceManager($serviceManagerConfig);
+            $this->serviceLocator = new ServiceManager($serviceManagerConfig);
         }
         return $this->serviceLocator;
     }
 
     /**
-     * 
+     *
      * @return UssdMenusServedMapper
      */
-    public function getUssdMenusServedMapper() {
+    public function getUssdMenusServedMapper()
+    {
         return $this->getServiceLocator()
-                        ->get(UssdMenusServedMapper::class);
+            ->get(UssdMenusServedMapper::class);
     }
 
     /**
-     * 
+     *
      * @return UssdLoopMapper
      */
-    public function getUssdLoopMapper() {
+    public function getUssdLoopMapper()
+    {
         return $this->getServiceLocator()
-                        ->get(UssdLoopMapper::class);
+            ->get(UssdLoopMapper::class);
     }
 
     /**
-     * 
+     *
      * @return SkippableUssdMenuMapper
      */
-    public function getSkippableUssdMenuMapper() {
+    public function getSkippableUssdMenuMapper()
+    {
         return $this->getServiceLocator()
-                        ->get(SkippableUssdMenuMapper::class);
+            ->get(SkippableUssdMenuMapper::class);
     }
 
     /**
-     * 
+     *
      * @return UssdService
      */
-    public function getUssdService($ussdText, $separator = "*") {
+    public function getUssdService($ussdText, $separator = "*")
+    {
 
         if (!$this->ussdService) {
-            $ussdService       = new UssdService($ussdText, $separator);
+            $ussdService = new UssdService($ussdText, $separator);
             $this->ussdService = $ussdService;
         }
         return $this->ussdService;
     }
 
     /**
-     * 
+     *
      * @return UssdResponseGenerator
      */
-    public function getUssdResponseGenerator() {
+    public function getUssdResponseGenerator()
+    {
         return $this->ussdResponseGenerator;
     }
 
     /**
      * Used to access functionality from the class that initializes InstantUssd
-     * 
+     *
      * $initializer = $ussdEvent
      *                  ->getTarget()->getInitializer();
-     * 
+     *
      * @return object
      */
-    public function getInitializer() {
+    public function getInitializer()
+    {
         return $this->initializer;
     }
 
     /**
      * Get attached logger or try to retreive one from ServiceManager
-     * 
+     *
      * @return object
      */
-    public function getLogger() {
+    public function getLogger()
+    {
 
         if (!$this->logger) {
-            $sl           = $this->getServiceLocator();
+            $sl = $this->getServiceLocator();
             // check we were able to get a service locator
             $this->logger = $sl->get('logger');
         }
@@ -322,20 +337,22 @@ class InstantUssd {
 
     /**
      * Directly attach your own logger
-     * 
+     *
      * @param object $logger
      * @return $this
      */
-    public function setLogger($logger) {
+    public function setLogger($logger)
+    {
         $this->logger = $logger;
         return $this;
     }
 
     /**
-     * 
+     *
      * @return EventManagerInterface
      */
-    public function getEventManager() {
+    public function getEventManager()
+    {
         if (!$this->eventManager) {
             if (empty($this->ussdEventManager)) {
                 throw new Exception('UssdEventManager class not set.');
@@ -347,10 +364,11 @@ class InstantUssd {
     }
 
     /**
-     * 
+     *
      * @param EventManagerInterface $eventManager
      */
-    protected function setEventManager(EventManagerInterface $eventManager) {
+    protected function setEventManager(EventManagerInterface $eventManager)
+    {
         $eventManager->setIdentifiers([__NAMESPACE__]);
         // set a custom ussd event
         $eventManager->setEventPrototype(new UssdEvent('ussd'));
@@ -358,10 +376,11 @@ class InstantUssd {
     }
 
     /**
-     * 
+     *
      * @param string $ussdEventManager
      */
-    protected function setUssdEventManager($ussdEventManager) {
+    protected function setUssdEventManager($ussdEventManager)
+    {
         if (!(gettype($ussdEventManager) === 'string')) {
             throw new \Exception('ussd_event_manager should be a string.');
         }
@@ -370,41 +389,44 @@ class InstantUssd {
 
     /**
      * get last served menu_id from database
-     * 
+     *
      * @param array $ussdData
      * @return mixed string|null
      */
-    public function retrieveLastServedMenuId(array $ussdData) {
-        $resultsMenuId    = $this->eventManager
-                ->trigger('_retreive_last_served_menu_', $this, $ussdData);
+    public function retrieveLastServedMenuId(array $ussdData)
+    {
+        $resultsMenuId = $this->eventManager
+            ->trigger('_retreive_last_served_menu_', $this, $ussdData);
         $lastServedMenuId = $resultsMenuId->first();
         return $lastServedMenuId;
     }
 
     /**
-     * 
+     *
      * @param array $ussdData
      * @return mixed ArrayObject|false|null
      */
-    public function retrieveMenuConfig(array $ussdData) {
+    public function retrieveMenuConfig(array $ussdData)
+    {
         $resultsMenuConfig = $this->eventManager
-                ->trigger('_retreive_menu_config_', $this, $ussdData);
-        $menuConfig        = $resultsMenuConfig->first();
+            ->trigger('_retreive_menu_config_', $this, $ussdData);
+        $menuConfig = $resultsMenuConfig->first();
         return $menuConfig;
     }
 
     /**
      * Send data for processing by the listener and returns pointer to the next screen
-     * 
+     *
      * @param string $lastServedMenuId
      * @param array $ussdData
      * @return mixed boolean|string pointer to the next screen or false
      */
-    public function processIncomingData($lastServedMenuId, array $ussdData) {
+    public function processIncomingData($lastServedMenuId, array $ussdData)
+    {
 
         // activate incoming data state
         $ussdData['is_incoming_data'] = true;
-        $incomingCycleResults         = $this->eventManager->triggerUntil(function ($result) {
+        $incomingCycleResults = $this->eventManager->triggerUntil(function ($result) {
             // data was processed and we should expect a pointer to the next menu
             return ($result instanceof UssdMenuItem);
         }, $lastServedMenuId, $this, $ussdData);
@@ -414,78 +436,80 @@ class InstantUssd {
         }
 
         // try and render the pointer/next screen
-        $ussdMenuItem              = $incomingCycleResults->last();
+        $ussdMenuItem = $incomingCycleResults->last();
         $isResetToPreviousPosition = $ussdMenuItem->isResetToPreviousPosition();
         // retreive our next menu_id
-        $nextScreenId              = $ussdMenuItem->getNextMenuId();
+        $nextScreenId = $ussdMenuItem->getNextMenuId();
         // check if it's a parent node reset
         if ($isResetToPreviousPosition) {
             $this->getUssdMenusServedMapper()
-                    ->resetMenuVisitHistoryToPreviousPosition($ussdData['session_id'], $nextScreenId);
+                ->resetMenuVisitHistoryToPreviousPosition($ussdData['session_id'], $nextScreenId);
         }
         // pointer to the next screen
         return $nextScreenId;
     }
 
     /**
-     * Check if we should continue looping a loopset or exit. It should be 
+     * Check if we should continue looping a loopset or exit. It should be
      * called before determining next menu. Should be called at the very top of
      * your listener
-     * 
+     *
      * @param ArrayObject $menuConfig
      * @param UssdEvent $e
      * @return boolean
      */
-    public function shouldStopLooping(ArrayObject $menuConfig, UssdEvent $e) {
+    public function shouldStopLooping(ArrayObject $menuConfig, UssdEvent $e)
+    {
 
         if (!array_key_exists('is_loop_end', $menuConfig) ||
-                empty($menuConfig['is_loop_end'])) {
+            empty($menuConfig['is_loop_end'])) {
             return true;
         }
         // we must have loopset_name to proceed
         if (!array_key_exists('loopset_name', $menuConfig) ||
-                empty($menuConfig['loopset_name'])) {
+            empty($menuConfig['loopset_name'])) {
             throw new Exception('loopset_name not set for menu_id '
-            . $e->getName() . ' ' . __FILE__ . ':' . __LINE__);
+                . $e->getName() . ' ' . __FILE__ . ':' . __LINE__);
         }
-        $loopsetName    = $menuConfig['loopset_name'];
-        $sessionId      = $e->getParam('session_id');
+        $loopsetName = $menuConfig['loopset_name'];
+        $sessionId = $e->getParam('session_id');
         $latestResponse = $e->getParam('latest_response');
 
-        $ussdLoopMapper    = $this->getUssdLoopMapper();
+        $ussdLoopMapper = $this->getUssdLoopMapper();
         $shouldStopLooping = $ussdLoopMapper->shouldStopLooping(
-                $loopsetName, $sessionId, $menuConfig);
+            $loopsetName, $sessionId, $menuConfig);
 
         if (!$shouldStopLooping
-                // prevent double increment due to double pass per listener
-                // Track count on the contains data face
-                && ($e->containsIncomingData())
-                // lastly prevent increment in loops done when user goes back
-                // this happens because event will be refired on post go back
-                && (trim($latestResponse) !== UssdService::GO_BACK_KEY)) {
+            // prevent double increment due to double pass per listener
+            // Track count on the contains data face
+            && ($e->containsIncomingData())
+            // lastly prevent increment in loops done when user goes back
+            // this happens because event will be refired on post go back
+            && (trim($latestResponse) !== UssdService::GO_BACK_KEY)) {
             $ussdLoopMapper->incrementLoops($loopsetName, $sessionId);
             // check AGAIN if we should stop looping
             // informs determineNextMenu
             //--- helpful incase of skippable menu
             $shouldStopLooping = $ussdLoopMapper->shouldStopLooping(
-                    $loopsetName, $sessionId, $menuConfig);
+                $loopsetName, $sessionId, $menuConfig);
         }
         return $shouldStopLooping;
     }
 
     /**
      * Clears all menu tracking history. Only called when user visits home page.
-     * 
+     *
      * Note - an application may have more than one home page
-     * 
+     *
      * @param UssdEvent $e
      * @return boolean
      */
-    public function clearMenuVisitHistory(UssdEvent $e) {
+    public function clearMenuVisitHistory(UssdEvent $e)
+    {
         // retreive mapper
         $ussdMenusServedMapper = $this->getUssdMenusServedMapper();
         // clear menu visit history
-        $result                = $ussdMenusServedMapper->clearMenuVisitHistoryBySessionId($e->getParam('session_id'));
+        $result = $ussdMenusServedMapper->clearMenuVisitHistoryBySessionId($e->getParam('session_id'));
         return $result;
     }
 
